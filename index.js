49f0e2125c1809316cfcb9ba3ec67322614830f2
+ <+>
2022-05-15 05:42:42 -0400

    commit tree c8e01ef083671d3ec0e14ffddb6b6dcb9e186c75

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+//test1
+const num1 = 31;
+const num2 = 2;
+let multiply = num1*num2;
+
+
+//test 2
+const random = Math.floor(Math.random()*10);
+
+
+//test 3
+const num3 = 29;
+const num4 = 5;
+const mod = num3 % num4;
+
+
+//test 4
+const max = Math.max(4, 3, 17, 19, 20);
